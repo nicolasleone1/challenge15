34c314e9a841f3123183d6bd515e4844630c1a2d
nicolas leone <leoneklyver@gmail.com>
2022-05-05 20:28:52 -0300

    guardando challenge15

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Crear un algoritmo por el cual se simule el lanzamiento de dos dados
+
+let dice1 = Math.round((Math.random())*6);
+let dice2 = Math.round((Math.random())*6);
+
+console.log(dice1);
+console.log(dice2);
+
